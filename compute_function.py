39f052180b6ffa30b8a8a524fe233bf87496020a
sush1 <pavelsirotkin@MacBook-Air.local>
2023-06-20 14:27:14 +0300

    fix: реализовал логику, прогнал тесты

--- compute_function.py
+++ compute_function.py
@@ -1,8 +1,18 @@
 from math import sqrt
 from typing import Union
 
-def solve(a: float, b: float, c: float) -> Union[float, float]:
-    raise NotImplementedError
+def solve(a: float, b: float, c: float, e: float=1e-5) -> Union[float, float]:
+    if abs(a) <= e:
+        raise ValueError("Argument a shouldn't be 0")
+    
+    d = b*b - 4*a*c
 
-
+    if  d < -e:
+        return []
+    elif abs(d) <= e:
+        return [-b/2*a, -b/2*a]
+    elif d > e:
+        x1 = (-b + sqrt(d)) / 2*a
+        x2 = (-b - sqrt(d)) / 2*a
+        return [x1, x2]
 
--- test_solve.py
+++ test_solve.py
@@ -4,8 +4,15 @@ from compute_function import solve
 def test_no_roots():
     assert solve(1,0,1) == []
 
-def test_equal_roots():
+def test_two_roots():
     assert solve(1,0,-1) == [1,-1]
 
-def test_():
-    assert solve(1,2,1) == [1,-1]
+def test_one_root():
+    assert solve(1,2,1) == [-1,-1]
+
+def test_a_equal_zero():
+    try:
+        solve(0, 1, 1)
+    except ValueError:
+        assert True
+        
