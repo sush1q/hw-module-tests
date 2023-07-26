9436c90620142695872983e46ea65d543ee2e8a6
sush1 <pavelsirotkin@MacBook-Air.local>
2023-07-26 20:41:46 +0300

    feat: реализовал тесты на типы, отличные от float

--- test_solve.py
+++ test_solve.py
@@ -1,3 +1,4 @@
+import pytest
 from compute_function import solve
 
 
@@ -12,7 +13,22 @@ def test_one_root():
 
 def test_a_equal_zero():
     try:
-        solve(0, 1, 1)
+        solve(0.0, 1, 1)
     except ValueError:
         assert True
-        
+
+def test_float_boundary_values_with_infinite():
+    assert solve(float('inf'), 1, 1) == []
+
+def test_float_boundary_values_with_minus_infinite():
+    assert solve(float('-inf'), 1, 1) == [float('-inf'), float('inf')]
+
+def test_float_boundary_values_with_nan():
+    assert solve(float('nan'), 1, 1) == None
+
+@pytest.mark.parametrize("value", ['str', [1, 2], {'a':1}, True, (1,2)])
+def test_unavailable_values(value):
+    try:
+        solve(value, 1, 1)
+    except TypeError:
+        assert True
